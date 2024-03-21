f44ad28d6646e74cb0fdd60199dde62a32972929
Krish Yadav <75037831+krrishexe@users.noreply.github.com>
2024-03-22 04:25:15 +0530

    updated cutTheSticks

--- arrays/cutTheSticks.js
+++ arrays/cutTheSticks.js
@@ -1,17 +1,22 @@
 function cutTheSticks(arr) {
     let count = [];
     let newArr = []
-
-    for (let m = 0; m < arr.length ; m++) {
-        const originalArr = arr.sort((a, b) => a - b)
-        count.push(originalArr.length)
+    let normie = arr;
+    for (let m = 0; m < 20; m++) {
+        const originalArr = normie.sort((a, b) => a - b)
+        if (originalArr.length >= 1) {
+            count.push(originalArr.length)
+        }
         let shortest = originalArr[0]
         let temp = []
         for (let i = 0; i < originalArr.length; i++) {
-            temp.push(arr[i] - shortest)
-            newArr = temp.filter(el => el > 0)
+            temp.push(originalArr[i] - shortest)
         }
-        console.log(newArr)
+        newArr = temp.filter(el => el > 0)
+        normie = newArr
+    }
+    for(let x =0;x<count.length;x++){
+        console.log(count[x])
     }
 }
 console.log(cutTheSticks([5, 4, 4, 2, 2, 8]))
